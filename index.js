7e5787c340a8f79c540afe4270b309d22d9f994b
battisti456 <battisti456@gmail.com>
2025-05-13 13:01:04 -0700

    step 3

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var typewriter = new Typewriter(app, {
 
 typewriter
   .pauseFor(1000)
-  .typeString('I am a software developer and content creator from the United States.') //Add your own tagline
+  .typeString('I am a Graduate Research Assistant studying ultrasound in veneer-based mass timber elements.') //Add your own tagline
   .pauseFor(3000)
   .start()
 
